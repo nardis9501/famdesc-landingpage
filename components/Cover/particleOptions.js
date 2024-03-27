a9ccf167ebdb0905f67deee98b6d23d0c2f1e4cb
Nardis <nardis9501@gmail.com>
2024-03-27 01:32:56 -0400

    feat: bounce particles on the screen

--- components/Cover/particleOptions.js
+++ components/Cover/particleOptions.js
@@ -10,7 +10,6 @@ export const optionsParticles = {
         enable: true,
         mode: "repulse",
       },
-      resize: true,
     },
     modes: {
       push: {
@@ -33,20 +32,19 @@ export const optionsParticles = {
       opacity: 0.5,
       width: 1,
     },
-    collisions: {
-      enable: true,
-    },
     move: {
-      direction: undefined,
+      direction: "none",
       enable: true,
-      random: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
       speed: 1,
       straight: false,
     },
     number: {
       density: {
         enable: true,
-        area: 800,
       },
       value: 80,
     },
