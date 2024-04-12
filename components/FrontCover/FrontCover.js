5b5f9ea07daab0c32a5fbb0cabb3ac5762a4ff17
Nardis <nardis9501@gmail.com>
2024-04-12 13:43:43 -0400

    feat: add transition with framer motion

--- components/FrontCover/FrontCover.js
+++ components/FrontCover/FrontCover.js
@@ -1,6 +1,8 @@
 "use client";
 import { useAppContext } from "@/app/providers";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/motionTransitions";
 
 export default function FrontCover(props) {
   const { color } = useAppContext();
@@ -16,7 +18,13 @@ export default function FrontCover(props) {
         }`}
       >
         <section className="md:grid grid-cols-2 h-full max-w-[1300px] px-2">
-          <div className="absolute top-0 left-0 mt-24 sm:mt-32 px-4 md:mt-0  md:relative z-10 flex flex-col h-full w-full">
+          <motion.div
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            animate="show"
+            exit={"hidden"}
+            className="absolute top-0 left-0 mt-24 sm:mt-32 px-4 md:mt-0  md:relative z-10 flex flex-col h-full w-full"
+          >
             <h1
               className={`font-medium my-3 text-4xl md:text-6xl lg:text-7xl text-black dark:text-slate-100`}
             >
@@ -33,8 +41,14 @@ export default function FrontCover(props) {
             >
               A closer and high value network
             </h2>
-          </div>
-          <div className="md:grid place-content-end drop-shadow-xl hidden">
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            animate="show"
+            exit={"hidden"}
+            className="md:grid place-content-end drop-shadow-xl hidden"
+          >
             <Image
               className="rounded-lg"
               src="/familie-cover.jpeg"
@@ -42,7 +56,7 @@ export default function FrontCover(props) {
               width={450}
               height={500}
             />
-          </div>
+          </motion.div>
         </section>
         <div className="inset-x-0 bottom-0 flex justify-center bg-gradient-to-t from-slate-50/95 pt-32 pb-8 pointer-events-none dark:from-slate-400 absolute"></div>
       </div>
