2b1fe917625a3f25e3f6180107f039ebedd3ce88
Nardis <nardis9501@gmail.com>
2024-04-12 12:51:57 -0400

    fix: change subtitle colors in light/dark mode

--- components/FrontCover/FrontCover.js
+++ components/FrontCover/FrontCover.js
@@ -9,32 +9,32 @@ export default function FrontCover(props) {
       <div
         className={`grid place-content-center drop-shadow-none pt-40 pb-20 bg-gradient-to-bl  to-slate-50 dark:to-slate-400 ${
           color === "blue"
-            ? "from-blue-500 via-blue-500/80"
+            ? "from-blue-500 via-blue-500/80 dark:via-blue-700"
             : color === "red"
             ? "from-red-500 via-red-500/80"
             : ""
         }`}
       >
-        <section className="grid grid-cols-2 h-full max-w-[1300px]">
-          <div className="flex flex-col justify-center">
+        <section className="md:grid grid-cols-2 h-full max-w-[1300px] px-2">
+          <div className="absolute top-0 left-0 mt-24 sm:mt-32 px-4 md:mt-0  md:relative z-10 flex flex-col h-full w-full">
             <h1
-              className={`flex justify-center font-medium my-3 text-7xl text-black dark:text-slate-100`}
+              className={`font-medium my-3 text-4xl md:text-6xl lg:text-7xl text-black dark:text-slate-100`}
             >
               The new Network for families
             </h1>
             <h2
-              className={`text-2xl ${
+              className={`text-2xl md:text-4xl font-medium text-black dark:text-slate-200 ${
                 color === "blue"
-                  ? "text-blue-500"
+                  ? "text-blue-500 md:text-blue-700"
                   : color === "red"
-                  ? "text-red-500"
+                  ? "text-red-500 md:text-red-700"
                   : ""
               }`}
             >
               A closer and high value network
             </h2>
           </div>
-          <div className="grid place-content-end drop-shadow-xl">
+          <div className="md:grid place-content-end drop-shadow-xl hidden">
             <Image
               className="rounded-lg"
               src="/familie-cover.jpeg"
