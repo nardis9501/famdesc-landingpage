24f1b0f483c3bf2b4256b8d8f1bf3517f5b6f049
Nardis <nardis9501@gmail.com>
2024-04-19 18:54:35 -0400

    fix: change image links in hardcode Store images used in avatar in local to avoid errors when changing them.

--- components/Testimonials/Testimonials.data.js
+++ components/Testimonials/Testimonials.data.js
@@ -2,7 +2,7 @@ export const TestimonialsData = [
   {
     id: 1,
     username: "Nardis Del Campo",
-    avatarUrl: "https://avatars.githubusercontent.com/u/48690649?v=4",
+    avatarUrl: "/avatar/nardis.webp",
     testimonial:
       "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
     job: "CEO at FamilyTree",
@@ -10,8 +10,7 @@ export const TestimonialsData = [
   {
     id: 2,
     username: "Liang Ricardo",
-    avatarUrl:
-      "https://media.licdn.com/dms/image/C4E03AQET8X-nNnTmCw/profile-displayphoto-shrink_800_800/0/1658289920385?e=1716422400&v=beta&t=DmGU1Kb-KCMdIxZTlHXyuGmYVCw_M3wsw62EcvizB34",
+    avatarUrl: "/avatar/liang.webp",
     testimonial:
       "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
     job: "Full Stack Developer",
@@ -19,8 +18,7 @@ export const TestimonialsData = [
   {
     id: 3,
     username: "Arian Milanes",
-    avatarUrl:
-      "https://media.licdn.com/dms/image/C5603AQFbNKunevLGHw/profile-displayphoto-shrink_800_800/0/1612466546160?e=1716422400&v=beta&t=FdquD0b4r0nMCkeg8ea1CTpU_od11FJ_adj4KOaJUVI",
+    avatarUrl: "/avatar/arian.webp",
     testimonial:
       "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
     job: "Full Stack Developer",
@@ -29,8 +27,7 @@ export const TestimonialsData = [
   {
     id: 4,
     username: "Frank Siret",
-    avatarUrl:
-      "https://media.licdn.com/dms/image/C5603AQFHg9RHJfHmBQ/profile-displayphoto-shrink_200_200/0/1650305815131?e=1717027200&v=beta&t=ECwyiz-LF3pT4vCuNs2gYZywyS4LAP_f1-uoRJBcoB0",
+    avatarUrl: "/avatar/frank.webp",
     testimonial:
       "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
     job: "React/Spring Developer at Peoplewalking",
