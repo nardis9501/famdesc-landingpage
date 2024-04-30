c5dbf97c1cb8f067cf493a8a889ed9128eeb2097
Nardis <nardis9501@gmail.com>
2024-04-30 04:22:52 -0400

    feat: add useful content

--- components/Video-Section/VideoSection.js
+++ components/Video-Section/VideoSection.js
@@ -13,7 +13,7 @@ export default function VideoSection(props) {
       >
         <div className="flex justify-center col-span-2">
           <h2 className="text-4xl font-semibold capitalize">
-            Created with you in mind: just right for you
+            Discover Famdesc: Our Vision and Core Values.
           </h2>
         </div>
         <div className="flex flex-col justify-center items-center my-10">
@@ -43,7 +43,6 @@ export default function VideoSection(props) {
         </div>
 
         <div className="flex flex-col my-10 md:ml-4 lg:ml-20 text-2xl lg:leading-9">
-          Built on the basis of these pillars:
           <ul className="space-y-4 text-left">
             <li className="flex items-center space-x-3 rtl:space-x-reverse">
               <svg
@@ -61,7 +60,7 @@ export default function VideoSection(props) {
                   d="M1 5.917 5.724 10.5 15 1.5"
                 />
               </svg>
-              <span>Increased social interaction</span>
+              <span>Free to All Users</span>
             </li>
             <li className="flex items-center space-x-3 rtl:space-x-reverse">
               <svg
@@ -79,7 +78,7 @@ export default function VideoSection(props) {
                   d="M1 5.917 5.724 10.5 15 1.5"
                 />
               </svg>
-              <span>Designed to give you the best possible experience</span>
+              <span>Open Source</span>
             </li>
             <li className="flex items-center space-x-3 rtl:space-x-reverse">
               <svg
@@ -97,7 +96,7 @@ export default function VideoSection(props) {
                   d="M1 5.917 5.724 10.5 15 1.5"
                 />
               </svg>
-              <span>Your privacy is our priority</span>
+              <span>High Quality Content</span>
             </li>
             <li className="flex items-center space-x-3 rtl:space-x-reverse">
               <svg
@@ -115,25 +114,7 @@ export default function VideoSection(props) {
                   d="M1 5.917 5.724 10.5 15 1.5"
                 />
               </svg>
-              <span>Committed to the truth</span>
-            </li>
-            <li className="flex items-center space-x-3 rtl:space-x-reverse">
-              <svg
-                className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 16 12"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5.917 5.724 10.5 15 1.5"
-                />
-              </svg>
-              <span>We care about our credibility and your trust</span>
+              <span>Data Privacy and Security</span>
             </li>
           </ul>
         </div>
