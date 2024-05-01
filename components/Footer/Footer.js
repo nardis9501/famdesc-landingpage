6dda793e8646cac3472cc632bf2ac6ce4e260093
Nardis <nardis9501@gmail.com>
2024-05-01 16:45:23 -0400

    fix: change link

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer(props) {
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2024{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
+            <a href="https://famdesc.com/" className="hover:underline">
               Famdesc LLC
             </a>
             . All Rights Reserved.
