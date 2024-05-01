7edd7df242f67fc54a1cb9cca508e65c645e0737
Nardis <nardis9501@gmail.com>
2024-05-01 18:10:57 -0400

    fix: change padding and margin

--- components/BenefitsList/FeatureCard.js
+++ components/BenefitsList/FeatureCard.js
@@ -27,7 +27,7 @@ export default function FeatureCard(props) {
             >
               <section
                 className={
-                  "p-8 md:p-6 my-20 m-4 md:mx-0  text-gray-950 dark:text-white"
+                  "p-2 md:p-6 my-20 md:mx-0  text-gray-950 dark:text-white"
                 }
               >
                 <h4 className="text-4xl font-bold mb-6">{title}</h4>
@@ -42,7 +42,7 @@ export default function FeatureCard(props) {
                     } mx-auto mb-8 md:mb-0 h-80 object-cover rounded-md`}
                   />
                   <p
-                    className={`md:mx-10 rounded-lg p-10 bg-white dark:backdrop-blur dark:bg-slate-900/30 dark:ring-1 dark:ring-inset dark:ring-white/10 shadow-xl`}
+                    className={`md:mx-10 rounded-lg p-4 bg-white dark:backdrop-blur dark:bg-slate-900/30 dark:ring-1 dark:ring-inset dark:ring-white/10 shadow-xl`}
                   >
                     {text}
                   </p>
