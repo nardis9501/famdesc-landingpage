571e6b20f1074de831ff5c0a7c0ed8d3458402a5
Nardis <nardis9501@gmail.com>
2024-05-01 19:04:59 -0400

    fix: change font weight

--- components/BenefitsList/FeatureCard.js
+++ components/BenefitsList/FeatureCard.js
@@ -30,7 +30,7 @@ export default function FeatureCard(props) {
                   "p-2 md:p-6 my-20 md:mx-0  text-gray-950 dark:text-white"
                 }
               >
-                <h4 className="text-4xl font-bold mb-6">{shortTitle}</h4>
+                <h4 className="text-4xl font-medium mb-6">{shortTitle}</h4>
                 <div className="grid grid-cols-1  md:grid-cols-2">
                   <Image
                     src={img}
