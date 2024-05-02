67f33b110260e9ed8a821475c3939686ae666eb1
Nardis <nardis9501@gmail.com>
2024-05-02 08:34:44 -0400

    feat: change testimonials

--- components/Testimonials/Testimonials.data.js
+++ components/Testimonials/Testimonials.data.js
@@ -4,15 +4,15 @@ export const TestimonialsData = [
     username: "Nardis Del Campo",
     avatarUrl: "/avatar/nardis.webp",
     testimonial:
-      "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
-    job: "CEO at FamilyTree",
+      "Famdesc is a digital platform dedicated to strengthening family bonds and preserving special moments over time.",
+    job: "CEO at Famdesc",
   },
   {
     id: 2,
     username: "Liang Ricardo",
     avatarUrl: "/avatar/liang.webp",
     testimonial:
-      "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
+      "With a user-centric approach and authenticity, offers a welcoming space where families can build, share and celebrate together.",
     job: "Full Stack Developer",
   },
   {
@@ -20,7 +20,7 @@ export const TestimonialsData = [
     username: "Arian Milanes",
     avatarUrl: "/avatar/arian.webp",
     testimonial:
-      "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
+      "On Famdesc, users have the opportunity to create and explore detailed family trees, connecting past and present generations. In addition, the platform facilitates the sharing of photos, videos and meaningful memories, allowing every family moment to be preserved and shared securely.",
     job: "Full Stack Developer",
   },
 
@@ -29,7 +29,7 @@ export const TestimonialsData = [
     username: "Frank Siret",
     avatarUrl: "/avatar/frank.webp",
     testimonial:
-      "FamilyTree will be an enhanced, open source, user-centric social network with content of value to everyone globally",
+      "Famdesc will be an enhanced, open source, user-centric social network with content of value to everyone globally",
     job: "React/Spring Developer at Peoplewalking",
   },
 ];
