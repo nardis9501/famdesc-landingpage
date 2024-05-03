3e8ec24a0a051cd768493d666e50ad9ced992f98
Nardis <nardis9501@gmail.com>
2024-05-02 22:53:37 -0400

    fix: add padding

--- components/BenefitsList/FeatureCard.js
+++ components/BenefitsList/FeatureCard.js
@@ -25,8 +25,8 @@ export default function FeatureCard(props) {
               backdropBottom
               heroImageNumber={counter}
             >
-              <section className="p-2 md:p-6 my-20 md:mx-0  text-gray-950 dark:text-white">
-                <h4 className="capitalize text-4xl font-bold mb-6">
+              <section className="p-4 md:p-6 my-20 md:mx-0 text-gray-950 dark:text-white">
+                <h4 className="capitalize text-3xl md:text-4xl font-bold mb-6">
                   {shortTitle}
                 </h4>
                 <div className="grid grid-cols-1  md:grid-cols-2">
