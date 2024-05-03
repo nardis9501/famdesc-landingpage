125b3e4b2e0b45ec0779326ac05c61372d4f61e1
Nardis <nardis9501@gmail.com>
2024-05-03 01:04:05 -0400

    refactor: refactoring of the page benefits For clearer and more concise communication, promoting better understanding and appealing to a wider audience.

--- components/BenefitsList/list.data.js
+++ components/BenefitsList/list.data.js
@@ -2,29 +2,40 @@ export const benefits = [
   {
     id: 0,
     title: "Strengthening family ties",
-    shortTitle: "Strengthening family ties",
-    text: "Famdesc allows families to connect, share special moments and stay in touch, strengthening family ties across time and distance.",
+    shortTitle: "family ties",
+    text1:
+      "It allows families to connect and share special moments, strengthening family ties across time and distance.",
+    text2:
+      "In addition, Famdesc offers the opportunity to stay in touch, facilitating continuous communication between family members, no matter where they are.",
     img: "/interaction.webp",
   },
   {
     id: 1,
     title: "Preservation of memories and family legacy",
-    shortTitle: "memories and family legacy",
-    text: "Users can share photos, videos and meaningful memories on Famdesc, creating a digital archive of family history that can be enjoyed by future generations.",
+    shortTitle: "family legacy",
+    text1: "Share meaningful moments.",
+    text2:
+      "Users can share photos, videos and special memories on Famdesc, creating a digital archive of family history that will live on for generations to come.",
     img: "/ui-ux.webp",
   },
   {
     id: 2,
     title: "Promoting authenticity and truth",
     shortTitle: "authenticity and truth",
-    text: "Famdesc is committed to promoting authenticity and eliminating hate speech by creating a safe and respectful environment where users can openly share their stories and experiences.",
+    text1:
+      "Famdesc promotes a safe environment where users can share their experiences openly.",
+    text2:
+      "We are committed to eliminating hate speech and encouraging authenticity in every interaction.",
     img: "/truth.webp",
   },
   {
     id: 3,
     title: "Facilitation of intergenerational interaction",
     shortTitle: "intergenerational interaction",
-    text: "The platform allows people of all ages to connect and share, thus promoting intergenerational interaction and the exchange of knowledge and experiences between different generations of the family.",
+    text1:
+      "Famdesc facilitates connections and sharing among people of all ages, fostering intergenerational interaction.",
+    text2:
+      " It promotes the exchange of knowledge and experiences between different generations within the family, enriching the collective wisdom of the family unit.",
     img: "/OpenSource.webp",
   },
 ];
@@ -34,28 +45,32 @@ export const features = [
     id: 0,
     title: "Increased interaction",
     shortTitle: "",
-    text: "",
+    text1: "",
+    text2: "",
     img: "/interaction.webp",
   },
   {
     id: 1,
     title: "User experience",
     shortTitle: "",
-    text: "",
+    text1: "",
+    text2: "",
     img: "/ui-ux.webp",
   },
   {
     id: 2,
     title: "Commitment to truth",
     shortTitle: "",
-    text: "",
+    text1: "",
+    text2: "",
     img: "/truth.webp",
   },
   {
     id: 3,
     title: "Credibility and confidence",
     shortTitle: "",
-    text: "",
+    text1: "",
+    text2: "",
     img: "/OpenSource.webp",
   },
 ];
