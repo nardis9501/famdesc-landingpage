193c518e45ef92668f480bbab39bcd5e747bbbeb
Nardis <nardis9501@gmail.com>
2024-05-07 23:48:50 -0400

    feat: update styles for a button

--- components/sponsor/SponsorButton.js
+++ components/sponsor/SponsorButton.js
@@ -5,21 +5,15 @@ export default function SponsorButton({ isWhite }) {
   return (
     <>
       <button
-        className={`h-12 w-full px-2 whitespace-nowrap md:h-14 md:w-36 rounded-lg ${
-          !isWhite && "text-white"
-        } text-xl md:text-xl font-semibold hover:bg-salmon ${
-          isWhite && color === "blue"
-            ? "bg-white text-blue-500"
-            : isWhite && color === "red"
-            ? "bg-white text-red-500"
-            : color === "blue"
+        className={` ${
+          color === "blue"
             ? "bg-blue-500"
             : color === "red"
             ? "bg-red-500"
-            : ""
-        }`}
+            : "bg-blue-500"
+        } mt-2 w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white dark:text-gray-900  hover:bg-salmon focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-gray-500`}
       >
-        Donate now
+        Donate Now
       </button>
     </>
   );
