5294957714bbcdde96ecbb8e28fc8564e8455a10
Nardis <nardis9501@gmail.com>
2024-05-08 00:07:27 -0400

    fix: remove margin top from the button

--- components/sponsor/SponsorButton.js
+++ components/sponsor/SponsorButton.js
@@ -11,7 +11,7 @@ export default function SponsorButton({ isWhite }) {
             : color === "red"
             ? "bg-red-500"
             : "bg-blue-500"
-        } mt-2 w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white dark:text-gray-900  hover:bg-salmon focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-gray-500`}
+        } w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white dark:text-gray-900  hover:bg-salmon focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-gray-500`}
       >
         Donate Now
       </button>
