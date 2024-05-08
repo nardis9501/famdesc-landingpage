5f3dfec6309470684eacab04b8f58f5736737c80
Nardis <nardis9501@gmail.com>
2024-05-08 00:48:07 -0400

    fix: change margin

--- components/BenefitsList/QuoteBanner.js
+++ components/BenefitsList/QuoteBanner.js
@@ -6,7 +6,7 @@ export default function QuoteBanner({ phrase }) {
   }
   return (
     <>
-      <div className="bg-white/30 dark:bg-gray-800/30 backdrop-blur-sm shadow-lg rounded-lg overflow-hidden mx-6 transform transition duration-500 hover:scale-105">
+      <div className="bg-white/30 dark:bg-gray-800/30 backdrop-blur-sm shadow-lg rounded-lg overflow-hidden mx-4 transform transition duration-500 hover:scale-105">
         <div className="p-6">
           <p className="text-gray-600 dark:text-gray-300 mb-6">{phrase}</p>
         </div>
